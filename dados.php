594951f44b1e1b19866373c8a299d506bf4c9157
leepifanio <leticiaepifanio@gmail.com>
2015-12-09 20:46:00 -0200

    arrumando

---
+++ dados.php
@@ -0,0 +1,25 @@
+<?php
+	echo "Nome: ".$_POST['campo_nome']."<br />";
+	echo "Email: ".$_POST['campo_email']."<br />";
+	echo "Assunto: ".$_POST['campo_assunto']."<br />";
+	echo "Mensagem: ".$_POST['campo_mensagem']."<br />";
+	
+
+	
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> Resposta </title>
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+	
+	<body>
+		<br />
+		<hr />
+	</body>
+</html>
